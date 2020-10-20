9e1dda6f9aca080ace0619fdf83fd65447ba4c5d
GarretCarmouche <garret307@yahoo.com>
2020-10-20 18:46:02 -0500

    Add deck data prompts

--- src/unoworkout/Main.java
+++ src/unoworkout/Main.java
@@ -1,16 +1,47 @@
 
 package unoworkout;
 
+import java.util.*;
 
 public class Main {
 
     
     public static void main(String args[]) {
-        Deck d = new Deck(1,true);
-        for(int i = 0; i < 107; i++){
-            Card c = d.drawCard();
-            //System.out.println(c.cardType+" "+c.cardColor);
-        }
+        int numberOfDecks = 1;
+        boolean includeActionCards;
+        boolean shuffleTogether;
+        String t;
+        boolean success = true;
+        
+        Scanner in = new Scanner(System.in);
+        
+        do{
+            System.out.print("Input the number of decks to shuffle: ");
+            t = in.next().trim();
+            try{
+                numberOfDecks = Integer.parseInt(t);
+            }catch(Exception ex){
+                success = false;
+            }
+                    
+        }while(!success);
+        
+        do{
+            System.out.print("Input whether they should be shuffled together (y/n) ");
+            t = in.next().trim().toLowerCase();
+            success = (t.equals("y") || t.equals("n"));
+            shuffleTogether = t.equals("y");
+        }while(!success);
+        
+        do{
+            success = true;
+            System.out.print("Input whether action cards should be included (y/n) ");
+            t = in.next().trim().toLowerCase();
+            success = (t.equals("y") || t.equals("n"));
+            includeActionCards = t.equals("y");
+        }while(!success);
+        
+        Deck deck = new Deck(numberOfDecks, includeActionCards, shuffleTogether);
     }
     
 }
