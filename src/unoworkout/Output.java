626f7d1b8cc91d46e935e54f320d440057b1f54e
angel-of-light <59181330+angel-of-light@users.noreply.github.com>
2020-10-27 18:12:39 -0500

    Output WIP

--- src/unoworkout/Output.java
+++ src/unoworkout/Output.java
@@ -2,5 +2,11 @@
 package unoworkout;
 
 public class Output {
+    // Create an array of Card object 
+    Card [] hand = new Card[7] ;
+    
+    // Create actual hands and make sure they have a color and a type 
+    
+    
     
 }
